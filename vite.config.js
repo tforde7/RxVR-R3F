9896555869f2347bfde3cea2fd64fd2dd4888759
tf <107359683@umail.ucc.ie>
2024-02-10 20:36:25 +0000

    added https config

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,11 @@ export default {
     server:
     {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+        https: {
+            key: './rxvr-privateKey.key',
+            cert: './rxvr.crt'
+        }
     },
     build:
     {
