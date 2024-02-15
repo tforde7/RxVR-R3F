d19e955d320717eff54cd2d3460ed77ebc893639
tf <107359683@umail.ucc.ie>
2024-02-15 16:59:28 +0000

    minor change

--- src/World/LectureTheatre.jsx
+++ src/World/LectureTheatre.jsx
@@ -15,7 +15,7 @@ const LectureTheatre = () => {
 
   const { position, rotation } = useControls('Lecture Theatre', {
     position: {
-      value: { x: 12.4, y: 5.3, z: -19.1 },
+      value: { x: 12.4, y: 5.4, z: -19.1 },
       step: 0.1,
     },
     rotation: { value: 0.28, step: 0.01 },
@@ -23,7 +23,7 @@ const LectureTheatre = () => {
 
   return (
     <mesh position={[position.x, position.y, position.z]} rotation-y={rotation}>
-      <boxGeometry args={[26.27, 10.6, 12]}></boxGeometry>
+      <boxGeometry args={[26.27, 10.8, 12]}></boxGeometry>
       <meshStandardMaterial map={colorTexture} normalMap={normalTexture}></meshStandardMaterial>
     </mesh>
   )
