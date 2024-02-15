c425b7d5bbd558d38318f1f39edee172baf7b44f
tf <107359683@umail.ucc.ie>
2024-02-15 20:36:04 +0000

    added textures for lobby

--- src/World/Lobby.jsx
+++ src/World/Lobby.jsx
@@ -1,4 +1,5 @@
 import { Base, Geometry, Subtraction } from '@react-three/csg'
+import { useLoader } from '@react-three/fiber'
 import { RigidBody } from '@react-three/rapier'
 import { useControls } from 'leva'
 import * as THREE from 'three'
@@ -29,7 +30,7 @@ const Lobby = () => {
     },
     doorPosition: {
       value: {
-        x: -11,
+        x: 0,
         y: -0.3,
         z: 0,
       },
@@ -37,31 +38,101 @@ const Lobby = () => {
     },
   })
 
+  const [wallColorTexture, wallAoTexture, wallDisplacementTexture, wallNormalTexture, wallRoughnessTexture] = useLoader(THREE.TextureLoader, [
+    '/textures/indoor/wall/color.jpg',
+    '/textures/indoor/wall/ao.jpg',
+    '/textures/indoor/wall/displacement.png',
+    '/textures/indoor/wall/normal.jpg',
+    '/textures/indoor/wall/roughness.jpg',
+  ])
+  const wallMaterial = new THREE.MeshStandardMaterial({
+    map: wallColorTexture,
+    aoMap: wallAoTexture,
+    normalMap: wallNormalTexture,
+    roughnessMap: wallRoughnessTexture,
+    side: THREE.DoubleSide,
+  })
+
+  const [floorColorTexture, floorAoTexture, floorDisplacementTexture, floorNormalTexture, floorRoughnessTexture] = useLoader(THREE.TextureLoader, [
+    '/textures/indoor/floor/color.jpg',
+    '/textures/indoor/floor/ao.jpg',
+    '/textures/indoor/floor/height.png',
+    '/textures/indoor/floor/normal.jpg',
+    '/textures/indoor/floor/roughness.jpg',
+  ])
+  const floorMaterial = new THREE.MeshStandardMaterial({
+    map: floorColorTexture,
+    aoMap: floorAoTexture,
+    normalMap: floorNormalTexture,
+    roughnessMap: floorRoughnessTexture,
+    side: THREE.DoubleSide,
+  })
+
+  const [ceilingColorTexture, ceilingAoTexture, ceilingDisplacementTexture, ceilingNormalTexture, ceilingRoughnessTexture] = useLoader(THREE.TextureLoader, [
+    '/textures/indoor/ceiling/color.jpg',
+    '/textures/indoor/ceiling/ao.jpg',
+    '/textures/indoor/ceiling/height.png',
+    '/textures/indoor/ceiling/normal.jpg',
+    '/textures/indoor/ceiling/roughness.jpg',
+  ])
+  const ceilingMaterial = new THREE.MeshStandardMaterial({
+    map: ceilingColorTexture,
+    aoMap: ceilingAoTexture,
+    normalMap: ceilingNormalTexture,
+    roughnessMap: ceilingRoughnessTexture,
+    side: THREE.DoubleSide,
+  })
+
+  const roomMaterials = [wallMaterial, wallMaterial, floorMaterial, ceilingMaterial, wallMaterial, wallMaterial]
+
   return (
-    <RigidBody colliders="trimesh" type="fixed">
-      <group position={[lobbyPosition.x, lobbyPosition.y, lobbyPosition.z]} rotation-y={rotation}>
-        <mesh position-y={3.6}>
-          <boxGeometry args={[22, 3.6, 20.5]}></boxGeometry>
-          <meshStandardMaterial></meshStandardMaterial>
-        </mesh>
-        <mesh>
-          <boxGeometry args={[22, 3.6, 20.5]}></boxGeometry>
-          <meshStandardMaterial></meshStandardMaterial>
-        </mesh>
-        <mesh position-y={-3.6}>
-          <Geometry>
-            <Base>
-              <boxGeometry args={[22, 3.6, 20.5]}></boxGeometry>
-            </Base>
-            <Subtraction position={[doorPosition.x, doorPosition.y, doorPosition.z]}>
-              <boxGeometry args={[1, size.height, size.width]}></boxGeometry>
-              {/* <meshStandardMaterial wireframe color={'red'}></meshStandardMaterial> */}
-            </Subtraction>
-          </Geometry>
-          <meshStandardMaterial side={THREE.DoubleSide}></meshStandardMaterial>
-        </mesh>
-      </group>
-    </RigidBody>
+    <>
+      <RigidBody colliders="trimesh" type="fixed">
+        <group position={[lobbyPosition.x, lobbyPosition.y, lobbyPosition.z]} rotation-y={rotation}>
+          <mesh position-y={3.6}>
+            <boxGeometry args={[22, 3.6, 20.5]}></boxGeometry>
+            <meshStandardMaterial></meshStandardMaterial>
+          </mesh>
+          <mesh>
+            <boxGeometry args={[22, 3.6, 20.5]}></boxGeometry>
+            <meshStandardMaterial></meshStandardMaterial>
+          </mesh>
+          <group position-y={-3.6}>
+            {/* left */}
+            <mesh position={[0, 0, -10.25]} material={wallMaterial}>
+              <planeGeometry args={[22, 3.6]} />
+            </mesh>
+            {/* right */}
+            <mesh position={[0, 0, 10.25]} material={wallMaterial}>
+              <planeGeometry args={[22, 3.6]} />
+            </mesh>
+            {/* front */}
+            <mesh rotation-y={Math.PI / 2} position={[-11, 0, 0]} material={wallMaterial}>
+              <Geometry>
+                <Base>
+                  <planeGeometry args={[20.5, 3.6]} />
+                </Base>
+                <Subtraction position={[doorPosition.x, doorPosition.y, doorPosition.z]}>
+                  <boxGeometry args={[size.width, size.height, 1]}></boxGeometry>
+                </Subtraction>
+              </Geometry>
+            </mesh>
+            {/* back */}
+            <mesh rotation-y={Math.PI / 2} position={[11, 0, 0]} material={wallMaterial}>
+              <planeGeometry args={[20.5, 3.6]} />
+            </mesh>
+            {/* ceiling */}
+            <mesh rotation-x={Math.PI / 2} position={[0, 1.79, 0]} material={ceilingMaterial}>
+              <planeGeometry args={[22, 20.5]} />
+            </mesh>
+            {/* floor */}
+            <mesh rotation-x={Math.PI / 2} position={[0, -1.8, 0]} material={floorMaterial}>
+              <planeGeometry args={[22, 20.5]} />
+            </mesh>
+          </group>
+        </group>
+      </RigidBody>
+    </>
   )
 }
 
