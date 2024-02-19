25710d8ab60adcd502d60f0cdd5a2a8e75bc8f26
tf <107359683@umail.ucc.ie>
2024-02-19 16:04:24 +0000

    moved x-ray
    (cherry picked from commit 90c4385d92a7b86756d73a5112367d5f3a575f3e)

--- src/World/XRayRoom.jsx
+++ src/World/XRayRoom.jsx
@@ -3,8 +3,16 @@ import { useLoader } from '@react-three/fiber'
 import { RigidBody } from '@react-three/rapier'
 import { Base, Geometry, Subtraction } from '@react-three/csg'
 import Room from './Room'
+import XRayBoard from './MainEntrance/XrayBoard'
 const XRayRoom = ({ length, width, height, position }) => {
-  return <Room length={20} width={20} height={3.6} position={[100, 0, 100]}></Room>
+  return (
+    <>
+      <Room length={20} width={20} height={3.6} position={[200, 0, -50]}></Room>
+      <group position={[200, 0, -50]}>
+        <XRayBoard></XRayBoard>
+      </group>
+    </>
+  )
 }
 
 export default XRayRoom
